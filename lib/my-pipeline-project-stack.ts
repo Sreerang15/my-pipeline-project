b054cb89c5d978a0efe1b57fc5fe4d93e8542d17
Sreerang <mandpesreerang@gmail.com>
2025-06-30 20:37:35 +0530

    first

--- lib/my-pipeline-project-stack.ts
+++ lib/my-pipeline-project-stack.ts
@@ -1,16 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  CodeBuildStep,
+} from 'aws-cdk-lib/pipelines';
+import * as cdk from 'aws-cdk-lib';
 
-export class MyPipelineProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MyPipelineProjectStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyPipelineProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyNewPipeline',
+      synth: new CodeBuildStep('SynthStep', {
+        input: CodePipelineSource.gitHub('Sreerang15/my-pipeline-project', 'master', {
+          authentication: cdk.SecretValue.unsafePlainText('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'), // OR use cdk.SecretValue.plainText(process.env.GITHUB_TOKEN!)
+        }),
+        installCommands: ['npm install'],
+        commands: ['npm run build', 'npx cdk synth'],
+      }),
+    });
   }
 }

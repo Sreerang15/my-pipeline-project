4646e3f6dfbe7ca484e9019c116f6c2ef38fe909
Sreerang <mandpesreerang@gmail.com>
2025-06-30 20:27:45 +0530

    Initial commit

---
+++ test/my-pipeline-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyPipelineProject from '../lib/my-pipeline-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-pipeline-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyPipelineProject.MyPipelineProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

fd2988f9f0cc2214c3b70bc77dd9762895b01be7
Sreerang <mandpesreerang@gmail.com>
2025-07-02 18:47:18 +0530

    qqq

--- lib/my-pipeline-project-stack.ts
+++ lib/my-pipeline-project-stack.ts
@@ -24,7 +24,7 @@ export class MyPipelineProjectStack extends Stack {
     })
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyNewPipeline',
+      pipelineName: 'MyNewPipeline2',
       synth: new CodeBuildStep('SynthStep', {
         input: CodePipelineSource.gitHub('Sreerang15/my-pipeline-project', 'master', {
           authentication: cdk.SecretValue.plainText('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'),

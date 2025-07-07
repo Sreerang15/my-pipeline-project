09b5e103bfccf2b954e8ae5f7b6ca1cc76178396
Sreerang <mandpesreerang@gmail.com>
2025-07-07 12:34:00 +0530

    yy

--- lib/my-pipeline-project-stack.ts
+++ lib/my-pipeline-project-stack.ts
@@ -50,8 +50,8 @@ export class MyPipelineProjectStacknew extends Stack {
       commands: ['npm run build', 'npx cdk synth'],
       rolePolicyStatements: [
     new cdk.aws_iam.PolicyStatement({
-      actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
-      resources: ['arn:aws:logs:ap-south-1:807157871082:log-group:*'],
+      actions: ['logs:*'],
+      resources: ['*'],
     }),
   ],
     });

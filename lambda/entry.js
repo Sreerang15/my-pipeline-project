961f29be60dd5aa89a37cb7cdc2f829ab51783cb
Sreerang <mandpesreerang@gmail.com>
2025-07-14 12:34:48 +0530

    ccc

--- lambda/entry.js
+++ lambda/entry.js
@@ -1,6 +1,6 @@
 // lambda/index.js
 exports.handler = async function (event) {
-  console.log("Hello from dummy lambda 3 latest kk 1");
+  console.log("Hello from dummy lambda 3 latest kk 1 bbb");
   return {
     statusCode: 200,
     body: "OK",

d640b2b445c6fce5f075cf9d0ff71972c23cc3f0
Sreerang <mandpesreerang@gmail.com>
2025-07-14 20:16:18 +0530

    popo

--- lib/my-pipeline-project-stack.ts
+++ lib/my-pipeline-project-stack.ts
@@ -109,10 +109,34 @@ export class MyPipelineProjectStacknew extends Stack {
       installCommands: ["npm install @aws-sdk/client-signer"],
       commands: ["echo 'Triggering Signer Job...'", "node sign-lambda.js"],
       rolePolicyStatements: [
-        new iam.PolicyStatement({
-          actions: ["s3:GetObject", "s3:PutObject"],
-          resources: ["arn:aws:s3:::my-sign-bucket/*"],
-        }),
+        new iam.PolicyStatement(
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: [
+              "signer:StartSigningJob",
+              "signer:DescribeSigningJob",
+              "s3:GetObject",
+              "s3:GetObjectVersion",
+              "s3:PutObject",
+            ],
+            resources: [
+              // Signer profile
+              "arn:aws:signer:ap-south-1:807157871082:signing-profile/MySigningProfileB3B3644B_66tketJzwLks",
+
+              // Source object for signing
+              "arn:aws:s3:::cdk-hnb659fds-assets-807157871082-ap-south-1/016f688f9bfdd3b74c15138d88d915a58711405a83c78142677d648778174053.zip",
+
+              // Optional: access to bucket for object version retrieval
+              "arn:aws:s3:::cdk-hnb659fds-assets-807157871082-ap-south-1",
+
+              // Destination prefix for signed artifact
+              "arn:aws:s3:::honorbucket/signed/*",
+
+              // Optional: root of destination bucket
+              "arn:aws:s3:::honorbucket",
+            ],
+          })
+        ),
       ],
     });
 
